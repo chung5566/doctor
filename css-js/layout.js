5427df4dfe82f2167be1b02d49dacd4423aac0cd
chung5566 <mac@chinritsugoutekiMacBook-Pro.local>
2015-08-19 16:00:42 +0800

    首頁三修

--- css-js/layout.js
+++ css-js/layout.js
@@ -7,7 +7,7 @@ $('.tab4s-himg').addClass('tab4s-addCSS1');
 $('.tab4s').each(function(){
 	$(this).mouseover(function(){
 		$(this).find('.tab4s-tit').stop().fadeOut(500);
-		$(this).find('.tab4s-img').stop().animate({top:'-100'}, 300);
+		$(this).find('.tab4s-img').stop().animate({top:'-240'}, 300);
 		$(this).find('.tab4s-img').addClass('tab4s-addCSS2');
 		$(this).find('.tab4s-himg').removeClass('tab4s-addHide');
 	});
